368782dc08be368e168d7b0cf11b94911c0aff26
René Wilhelm <rene.wilhelm@gmail.com>
2014-11-03 15:22:55 +0100

    Stub db entry

--- config/database/index.js
+++ config/database/index.js
@@ -9,10 +9,27 @@
        if (!!err) {
          console.log('An error occurred while creating the table:', err);
        } else {
-         console.log('Database schemes synchronized.');
+        console.log('Database schemes synchronized.');
+
+        db.User.create({
+          email: 'asdf@uni-koblenz.de',
+          firstName: 'René',
+          organization: 'Universität Koblenz-Landau, Campus Koblenz',
+          lastName: 'Wilhelm',
+          phone: '0160 8040042',
+          username: 'asdf'
+        })
+        .success(function(user) {
+          debugger
+        })
+        .error(function(error) {
+        });
+
        }
     });
+
   };
+
 
   // TODO return a promise here to be safe?
   module.exports = exports = main;
