ecae42873a9d6b094b34b12a110163ab5a20305a
René Wilhelm <rene.wilhelm@gmail.com>
2014-11-05 03:31:09 +0100

    More properties on user model

--- config/database/models/user.js
+++ config/database/models/user.js
@@ -1,42 +1,64 @@
 (function() {
   'use strict';
 
-  var crypt = require('bcrypt');
+  var bcrypt = require('bcrypt');
 
   module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define('User', {
-      jwtId:        DataTypes.STRING,
-      email:        DataTypes.STRING,
-      username:     DataTypes.STRING,
+      jwt_id: DataTypes.STRING,
+
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
       password: {
         type: DataTypes.STRING,
         set: function(v) {
-          var salt = bcrypt.genSaltSync(10);
-          var hash = bcrypt.hashSync(v, salt);
-
-          this.setDataValue('password', hash);
+          bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(v, salt, function(err, hash) {
+              this.setDataValue('password', hash);
+            }.bind(this));
+          }.bind(this));
         }
       },
-      lastName:     DataTypes.STRING,
-      firstName:    DataTypes.STRING,
-      organization: DataTypes.STRING,
-      enabled:      DataTypes.STRING,
-      phone:        DataTypes.STRING,
+
+      lname: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+
+      fname: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+
+      organization: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+
+      enabled: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+
     }, {
       paranoid: true,
       instanceMethods: {
-        comparePassword: function(candidatePassword, cb) {
-          bcrypt.compare(candidatePassword, this.getDataValue('password'), function(err, isMatch) {
+        comparePassword: function(v, cb) {
+          bcrypt.compare(v, this.getDataValue('password'), function(err, isMatch) {
             if(err) return cb(err);
             cb(null, isMatch);
           });
         },
-        // setPassword: function(v) {
-        //   var salt = bcrypt.genSaltSync(10);
-        //   var hash = bcrypt.hashSync(v, salt);
-
-        //   this.setDataValue('password', hash);
-        // },
         setToken: function() {
           // bla bla bla
           // bla bla bla
