2d4c70b1c36b6a3b684fdc5934fd240249f63f75
René Wilhelm <rene.wilhelm@gmail.com>
2014-11-10 04:43:34 +0100

    Refactored config file

---
+++ server/platform/config.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  module.exports = exports = {
+
+    development: {
+      app: {
+        port: process.env.PORT || 3000,
+        env: process.env.NODE_ENV || 'development'
+      },
+      db: {
+        postgres: {
+          url: process.env.DB_URL || 'postgres://localhost/bidos_development'
+        }
+      },
+      secret: {
+        key: process.env.SECRET_KEY || 'quaiQu8Aejee8MaikohdooSasohhiechieQu0idohthoo8Theesaepu6Tuc2yeed', // jwt secret
+        iv:  process.env.SECRET_IV  || 'aiXohhaaf5shaeyi6ash7Eethei4aechi0eelahy3aezaeN1Quai6yeeKooneum2' // ?
+      }
+    }
+
+  };
+
+  // module.exports = {
+  //   database: function(env) {
+  //     return _.merge(config.database.defaults, config.database[env]);
+  //   },
+  //   app: config.app,
+  //   config: config
+  // };
+
+}());
