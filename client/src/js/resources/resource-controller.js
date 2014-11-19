f42fb568be362e6850338840820ae3ee13fd8f6a
René Wilhelm <rene.wilhelm@gmail.com>
2014-11-19 01:53:31 +0100

    Resource modules (ctrl + service, was crud)

---
+++ client/src/js/resources/resource-controller.js
@@ -0,0 +1,62 @@
+(function() {
+  'use strict';
+
+  var _ = require('lodash');
+
+  require('./resource-constants');
+  require('./resource-services');
+
+  angular.module('bidos.resource.controller', [
+    'bidos.resource.constants',
+    'bidos.resource.services'
+  ])
+
+  .controller('resourceCtrl', ['$scope', '$rootScope', 'resourceService', '$state', 'RESOURCE_ACCESS',
+    function($scope, $rootScope, resourceService, $state, RESOURCE_ACCESS) {
+
+    console.log($scope);
+
+    var vm = this;
+    vm.resources = {};
+
+    // post form data
+    vm.create = function(resource, formData) {
+      console.info(resource + ':vm.read', formData);
+      resourceService.create(resource, formData).then(function(response) {
+        _.merge(vm.resources, response);
+      }, handleError);
+    };
+
+    // get 1 or * (id is optional)
+    vm.read = function(resource, id) {
+      console.info(resource + ':vm.read');
+      resourceService.read(resource, id).then(function(response) {
+        _.merge(vm.resources, response);
+      }, handleError);
+    };
+
+    // post form data
+    vm.update = function(resource, id, formData) {
+      console.info(resource + ':vm.update');
+      resourceService.update(resource, id, formData).then(function(response) {
+        // update vm
+      }, handleError);
+    };
+
+    // destroy in vm and db
+    vm.destroy = function(resource, id) {
+      console.info(resource + ':vm.destroy');
+      resourceService.destroy(resource, id).then(function(response) {
+        // update vm
+      }, handleError);
+    };
+
+    // init: get everything we are allowed to get
+    vm.read(RESOURCE_ACCESS[$rootScope.user.role].resources);
+
+    function handleError(response) {
+      console.warn('Error: ' + response.data);
+    }
+
+  }]);
+}());
