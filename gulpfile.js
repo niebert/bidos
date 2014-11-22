478f6eab8591aea20785a5d1257a5aa9684be0a7
René Wilhelm <rene.wilhelm@gmail.com>
2014-11-22 05:23:59 +0100

    Uglify

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,13 @@
        plumber = require('gulp-plumber'),
        nodemon = require('gulp-nodemon'),
        source = require('vinyl-source-stream'),
-       // uglify = require('gulp-uglify'),
-       // buffer = require('vinyl-buffer'),
+       uglify = require('gulp-uglify'),
+       buffer = require('vinyl-buffer'),
        browserify = require('gulp-browserify'),
        prefix  = require('gulp-autoprefixer'),
        ngAnnotate = require('gulp-ng-annotate'),
+       rename = require('gulp-rename'),
+       savefile = require('gulp-savefile'),
        sass = require('gulp-ruby-sass');
 
   var onError = function(err) {
@@ -18,44 +20,43 @@
     console.log(err);
   };
 
-  var sourceDir = 'client/src';
-  var targetDir = 'client/build';
+  var sourceDir = 'client/src/';
+  var targetDir = 'client/build/';
 
-  gulp.task('browserify', function() {
-    // Single entry point to browserify
-    gulp.src(sourceDir + '/js/app.js')
-      .pipe(ngAnnotate())
-      .pipe(browserify({
-        insertGlobals : true,
-        debug : !gulp.env.production // FIXME deprecated -> yargs, minimist
-      }))
-      .pipe(gulp.dest(targetDir + '/js'));
+  gulp.task('handleJs', function() {
+    gulp.src(sourceDir + 'app.js') // <-- single entry point
+    .pipe(browserify({ insertGlobals: true }))
+    .pipe(ngAnnotate())
+    .pipe(gulp.dest(targetDir))
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest(targetDir));
   });
 
-  gulp.task('sass', function() {
-    gulp.src(sourceDir + '/css/*.scss')
-      .pipe(plumber({errorHandler:onError}))
-      .pipe(sass({sourcemap:false, style:'compressed'}))
+  gulp.task('handleScss', function() {
+    gulp.src(sourceDir + 'stylesheets/app.scss')
+      .pipe(plumber({ errorHandler: onError }))
+      .pipe(sass({ style: 'compressed' }))
       .pipe(prefix(['last 1 version', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-      .pipe(gulp.dest(targetDir + '/css'));
+      .pipe(gulp.dest(targetDir));
   });
 
   gulp.task('watch', function() {
-    gulp.watch(sourceDir + '/**/*.js', ['browserify']);
-    gulp.watch(sourceDir + '/css/**/*.scss', ['sass']);
+    gulp.watch(sourceDir + '/**/*.js', ['handleJs']);
+    gulp.watch(sourceDir + '/**/*.scss', ['handleScss']);
   });
 
   gulp.task('serve', function() {
     nodemon({
       script: 'index.js',
       watch: ['index.js', 'platform'],
-      nodeArgs: ['--harmony'],
-      env: { NODE_ENV: 'development' } // FIXME deprecated -> yargs, minimist
+      env: { NODE_ENV: 'development' }, // FIXME deprecated -> yargs, minimist
+      nodeArgs: ['--harmony']
     })
     .on('change', []);
   });
 
-  gulp.task('development', ['browserify', 'sass', 'watch', 'serve']);
+  gulp.task('development', ['handleJs', 'handleScss', 'watch', 'serve']);
   gulp.task('default', ['development']);
 
 }());
