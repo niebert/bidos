c28cc04bc0dae2586020f293559c801d00e34778
René Wilhelm <rene.wilhelm@gmail.com>
2014-12-09 01:13:48 +0100

    Changed API host wqurl

--- client/src/app.js
+++ client/src/app.js
@@ -16,8 +16,11 @@
   ])
 
   // app wide constants
-  // .constant('API_URL', 'http://141.26.69.238/bidos')
-  .constant('API_URL', 'http://localhost:3000')
+  // .constant('API_URL', 'http://141.26.69.238/bidos') // lg
+  // .constant('API_URL', 'http://localhost:3000')
+  // .constant('API_URL', 'http://lvps92-51-147-239.dedicated.hosteurope.de:3000') // ch
+  // .constant('API_URL', 'http://92.51.147.239:3000') // ch
+  .constant('API_URL', 'http://bidos.sci-hub.ir:3000') // ch + ydns
   .constant('API_PATH', '/v1')
   .constant('ALLOWED_RESOURCES', {
     // admin: ['user', 'group', 'kid', 'domain', 'subdomain', 'item', 'example', 'behaviour']
