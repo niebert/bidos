e62e4c6d67681f867016c66973caa5e54e34d113
René Wilhelm <rene.wilhelm@gmail.com>
2015-02-02 15:02:25 +0100

    Client config

--- client/config.js
+++ client/config.js
@@ -4,10 +4,11 @@
   module.exports = {
 
     // https://color.adobe.com/explore/most-popular/?time=all
-
+    // http://codepen.io/benknight/full/nADpy/
     colors: {
       firenze: ['#337754', '#ffed94', '#ffa029', '#ac3619', '#7f1b00'],
-      phaedra: ['#ff4a25', '#ffff8b', '#b0e88e', '#63b17d', '#009376']
+      phaedra: ['#ff4a25', '#ffff8b', '#b0e88e', '#63b17d', '#009376'],
+      custom1: ['#f7c6ff', '#fff7b4', '#3a90ff', '#ffc9af', '#c8ffd6']
     }
 
   };
