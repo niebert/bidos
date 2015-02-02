1d83b29125c006c03ab4efc99cc492c33e5dd4da
René Wilhelm <rene.wilhelm@gmail.com>
2015-02-02 15:04:38 +0100

    Abstracted capture service

--- client/bidos-core/services/CaptureService.js
+++ client/bidos-core/services/CaptureService.js
@@ -8,70 +8,103 @@
 
   function CaptureService($rootScope, ResourceService, $q, $state) {
 
-    var observation = {};
+    console.log('%cCAPTURE SERVICE    : ' + $state.params.type, 'color: #333; font-weight: 500; font-size: 1.2em;');
 
-    return {
-      get: getObservation,
-      complete: completeObservation,
-      select: selectResource,
-    };
-
-    function selectResource(resource) {
-      var type = resource.type;
-      observation[type] = resource;
-      console.log(type);
-      switch (type) {
-        case 'kid':
-          $state.go('auth.capture');
-          break;
-        case 'domain':
-          $state.go('auth.select', {
-            type: 'subdomain'
-          });
-          break;
-        case 'subdomain':
-          $state.go('auth.select', {
-            type: 'item'
-          });
-          break;
-        case 'item':
-          $state.go('auth.select', {
-            type: 'behaviour'
-          });
-          break;
-        case 'behaviour':
-          if (resource.niveau === 0 || resource.niveau === 4) {
-            $state.go('auth.finish-observation');
-          } else {
-            $state.go('auth.select', {
-              type: 'help'
-            });
-          }
-          break;
-        case 'help':
-          $state.go('auth.finish-observation');
-          break;
-        case 'example':
-        case 'idea':
-          // push into array, create if neccessary
-          (observation[type] = observation[type] || [])
-          .push(resource);
-          break;
-        default:
-          $state.go('auth.capture');
+    var Observation = function() {
+      function reset() {
+        this.examples = [];
+        this.ideas = [];
       }
 
-      console.info(observation);
+      this.order = ['kid', 'domain', 'subdomain', 'item', 'behaviour', 'help', 'examples', 'ideas', 'review'];
+      this.current = 0;
+
+      function next() {
+        // var upwardsResources = this.order.slice(0, this.order.indexOf(this.current));
+
+        // _.each(upwardsResources.reverse(), function(d) {
+        //   console.log(upwardsResources, d);
+        //   if (!observation.hasOwnProperty(d)) {
+        //     console.log('%cGO_' + type, 'color: #1d4cbd; font-weight: 500; font-size: 1.2em;', observation);
+        //     this.go(d);
+        //   }
+        // }, this);
+      }
+
+      function prev() {
+        console.log('go to prev');
+      }
+
+      return {
+        examples: [],
+        ideas: [],
+        reset: reset,
+        next: next,
+        prev: prev,
+        order: this.order
+      };
+    };
+
+    var observation = new Observation();
+
+    return {
+      get: get,
+      done: done,
+      select: select,
+      add: add,
+      go: go
+    };
+
+    function add(resource) {
+      return $q(function(resolve) {
+        // push into array, create if neccessary
+        (observation[resource.type] = observation[resource.type] || [])
+        .push(resource);
+
+        resolve(observation);
+      });
     }
 
-    function getObservation() {
+    function cleanUp(resourceTypes) {
+      _.each(resourceTypes, function(type) {
+        delete observation[type];
+      });
+    }
+
+    function go(type) {
+      return $q(function(resolve) {
+        if (arguments.length === 0) {
+          return;
+        }
+        $state.go('auth.capture.go', {
+          type: type
+        });
+        resolve();
+      });
+    }
+
+    function select(resource) {
+
+      // <-- upwards / downwards -->
+      var type = resource.type;
+      observation[type] = resource;
+
+      // var downwardsResources = a.slice(a.indexOf(type) + 1, a.length);
+
+      // console.log('%cSEL_' + type, 'color: #1d4cbd; font-weight: 500; font-size: 1.2em;', resource);
+      // console.log('%ccleaning up', 'color: #333; font-weight: 500; font-size: 1.2em;', JSON.stringify(a));
+
+      // cleanUp(downwardsResources);
+
+    }
+
+    function get() {
       return $q(function(resolve) {
         resolve(observation);
       });
     }
 
-    function completeObservation() {
-
+    function done() {
       var obs = {
         type: 'observation',
         item_id: observation.item.id,
@@ -109,8 +142,8 @@
       ResourceService.create(obs)
         .then(function(response) {
           console.log(response);
-          $state.go('auth.capture');
-          observation = {};
+          this.go();
+          observation.reset();
         });
     }
 
