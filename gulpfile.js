e82f02c847b27b55711afcbe548d5a098b920d89
René Wilhelm <rene.wilhelm@gmail.com>
2015-02-11 01:01:54 +0100

    Package updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,20 @@
 
   var gulp = require('gulp');
   var chalk = require('chalk');
-  var util = require('gulp-util');
   var uglify = require('gulp-uglify');
-  var rename = require('gulp-rename');
   var sass = require('gulp-ruby-sass');
-  var plumber = require('gulp-plumber');
+  var concat = require('gulp-concat');
   var nodemon = require('gulp-nodemon');
+  var traceur = require('gulp-traceur');
   var browserify = require('browserify');
   var prefix = require('gulp-autoprefixer');
   var transform = require('vinyl-transform');
+  var sourcemaps = require('gulp-sourcemaps');
   var ngAnnotate = require('gulp-ng-annotate');
   var shell = require('gulp-shell');
 
   var sourceDir = 'client/';
   var targetDir = 'build/';
-
 
   gulp.task('scripts', scripts);
   gulp.task('stylesheets', stylesheets);
@@ -27,12 +26,6 @@
   gulp.task('development', ['scripts', 'stylesheets', 'manifest', 'watch', 'serve']);
   gulp.task('production', ['scripts', 'stylesheets', 'serve']);
   gulp.task('default', ['development']);
-
-
-  function onError(err) {
-    util.beep();
-    console.log(err);
-  }
 
   function manifest() {
     shell.task([
@@ -47,35 +40,32 @@
     });
 
     return gulp.src(sourceDir + 'app.js') // (['./src/*.js'])
+      .pipe(sourcemaps.init())
       .pipe(browserified)
       .pipe(ngAnnotate())
+      .pipe(traceur())
       .pipe(gulp.dest(targetDir))
       .pipe(uglify())
-      .pipe(rename('app.min.js'))
+      .pipe(concat('app.min.js'))
+      .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest(targetDir));
   }
 
-
   function stylesheets() {
-    gulp.src(sourceDir + 'stylesheets/app.scss')
-      .pipe(plumber({
-        errorHandler: onError
-      }))
-      .pipe(sass({
-        style: 'compressed'
-      }))
+    return sass(sourceDir + 'stylesheets/app.scss')
+      .on('error', function(err) {
+        console.error(err.message);
+      })
       .pipe(prefix(['last 2 version', '> 1%', 'ie 8', 'ie 7'], {
         cascade: true
       }))
       .pipe(gulp.dest(targetDir));
   }
 
-
   function watch() {
     gulp.watch(sourceDir + '/**/*.js', ['scripts']);
     gulp.watch(sourceDir + '/**/*.scss', ['stylesheets']);
   }
-
 
   function serve() {
     nodemon({
