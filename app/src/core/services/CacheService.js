02c500a80cd065ee044a450683df66e780de28b5
René Wilhelm <rene.wilhelm@gmail.com>
2015-02-26 05:23:20 +0100

    Prepare resources in cache service

--- app/src/core/services/CacheService.js
+++ app/src/core/services/CacheService.js
@@ -175,10 +175,227 @@
         });
       });
 
+      addItemHandlers(data);
+      addObservationHandlers(data);
+      addKidHandlers(data);
+      addGroupHandlers(data);
+      addInstitutionHandlers(data);
+      addUserHandlers(data);
+
       console.timeEnd('prepare cached data');
       return data;
-
     }
+
+    function addItemHandlers(data) {
+      _.each(data.items, function(item) {
+
+        // the next two require the item to have the get behaviours property
+        // to be set
+
+        if (!item.hasOwnProperty('examples')) {
+          Object.defineProperty(item, 'examples', {
+            get: function() {
+              return _.chain(this.behaviours)
+                .map('examples')
+                .flatten()
+                .value();
+            }
+          });
+        }
+
+        if (!item.hasOwnProperty('ideas')) {
+          Object.defineProperty(item, 'ideas', {
+            get: function() {
+              return _.chain(this.behaviours)
+                .map('ideas')
+                .flatten()
+                .value();
+            }
+          });
+        }
+
+      });
+    }
+
+    function addObservationHandlers(data) {
+      _.each(data.observations, function(observation) {
+        if (!observation.hasOwnProperty('behaviour')) {
+          Object.defineProperty(observation, 'behaviour', {
+            get: function() {
+              return _.filter(this.item.behaviours, {
+                niveau: this.niveau
+              })[0];
+            }
+          });
+        }
+      });
+    }
+
+    function addKidHandlers(data) {
+      _.each(data.kids, function(kid) {
+        if (!kid.hasOwnProperty('skill')) {
+          Object.defineProperty(kid, 'skill', {
+            get: function() {
+              var skill = [
+                _.chain(kid.observations).filter({
+                  domain_id: 1
+                }).map('niveau').reduce(function(sum, n) {
+                  return sum + n;
+                }, 0).value(),
+                _.chain(kid.observations).filter({
+                  domain_id: 2
+                }).map('niveau').reduce(function(sum, n) {
+                  return sum + n;
+                }, 0).value(),
+                _.chain(kid.observations).filter({
+                  domain_id: 3
+                }).map('niveau').reduce(function(sum, n) {
+                  return sum + n;
+                }, 0).value(),
+                _.chain(kid.observations).filter({
+                  domain_id: 4
+                }).map('niveau').reduce(function(sum, n) {
+                  return sum + n;
+                }, 0).value()
+              ];
+              return skill;
+            }
+          });
+        }
+
+        if (!kid.hasOwnProperty('age')) {
+          Object.defineProperty(kid, 'age', {
+            get: function() {
+              var today = new Date();
+              var birthDate = this.bday;
+              var age = today.getFullYear() - birthDate.getFullYear();
+              var m = today.getMonth() - birthDate.getMonth();
+              if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                age--;
+              }
+              return age;
+            }
+          });
+        }
+      });
+    }
+
+    function addGroupHandlers(data) {
+      _.each(data.groups, function(group) {
+        if (!group.hasOwnProperty('observations')) {
+          Object.defineProperty(group, 'observations', {
+            get: function() {
+              return _.chain(group.kids)
+                .map('observations')
+                .filter(function(d) {
+                  return d.length;
+                })
+                .value();
+            }
+          });
+        }
+      });
+    }
+
+    function addInstitutionHandlers(data) {
+
+      _.each(data.institutions, function(institution) {
+        if (!institution.hasOwnProperty('observations')) {
+          Object.defineProperty(institution, 'observations', {
+            get: function() {
+              return _.chain(institution.groups)
+                .map('kids')
+                .flatten()
+                .map('observations')
+                .filter(function(d) {
+                  return d.length;
+                })
+                .value();
+            }
+          });
+        }
+      });
+    }
+
+    function addUserHandlers(data) {
+
+      _.each(data.users, function(user) {
+        if (!user.hasOwnProperty('kids')) {
+          Object.defineProperty(user, 'kids', {
+            get: function() {
+              return _.chain(user.groups)
+                .map('kids')
+                .flatten()
+                .value();
+            }
+          });
+        }
+        if (!user.hasOwnProperty('roleName')) {
+          Object.defineProperty(user, 'roleName', {
+            get: function() {
+              switch (this.role) {
+                case 0:
+                  return 'admin';
+                case 1:
+                  return 'practitioner';
+                case 2:
+                  return 'scientist';
+              }
+            }
+          });
+        }
+      });
+    }
+
+    function makeRoleModifications(data) {
+      switch ($rootScope.auth.role) {
+
+        // ADMIN
+        case 0:
+          break;
+
+          // PRACTITIONER
+        case 1:
+          // put user specific kids and group resources on top scope level
+          data.kids = _.flatten(_.chain(data.users)
+            .filter({
+              id: $rootScope.auth.id
+            })
+            .first()
+            .value()
+            .institution.groups.map(function(d) {
+              return d.kids;
+            }));
+
+          data.groups = _.flatten(_.chain(data.users)
+            .filter({
+              id: $rootScope.auth.id
+            })
+            .first()
+            .value()
+            .institution.groups);
+
+          // data.observations = _.flatten(_.chain(data.users)
+          //   .filter({
+          //     id: $rootScope.auth.id
+          //   })
+          //   .first()
+          //   .value()
+          //   .institution.groups);
+
+          break;
+
+        case 2:
+          _.each(data.kids, function(k) {
+            var f = k.name.split(' ')[0]; // first name
+            k.name = f[0] + f[1] + f[f.length - 1] + k.id; // <3
+            console.log(k.name);
+          });
+          // debugger
+          break;
+      }
+    }
+
     function createResourcesInCache(resources) {
       return $q(function(resolve, reject) {
         _.each(resources, function(resource) {
