486fd72d145b32c69ae685263d65395bb61325ee
René Wilhelm <rene.wilhelm@gmail.com>
2015-03-09 03:46:58 +0100

    [fix] table / item resource

--- app/src/core/directives/bx-table/bx-table.js
+++ app/src/core/directives/bx-table/bx-table.js
@@ -18,7 +18,7 @@
       }
     };
 
-    function controller(Resources, $mdDialog, $scope, $rootScope, $http) {
+    function controller(Resources, CRUD, $mdDialog, $scope, $rootScope, $http) {
       var vm = angular.extend(this, {
         dialog: dialog,
         viewFilter: viewFilter,
@@ -112,7 +112,10 @@
             vm[resource.type] = response;
             resource = response;
             $mdDialog.hide();
-            vm.parent.users.splice(_.findIndex(vm.parent.users, { id: response.id }), 1, response);
+            debugger
+            vm.parent.users.splice(_.findIndex(vm.parent.users, {
+              id: response.id
+            }), 1, response);
           }).error(function(err) {
             console.error(err);
           });
@@ -127,29 +130,43 @@
           }
         }
 
+        // This is neccessary as we don't want to edit the resource directly
+        // (JS assigns by reference), but a copy so we can easily cancel the
+        // dialog w/o the need to revert any changes. Note that
+        // enumerable:true must be set when defining the getter, else the
+        // getters won't be cloned here (as flat props, though).
+
         vm[resource.type] = _.clone(resource);
 
-        vm.r = resource;
+        switch (resource.type) {
+          case 'item':
+            if (resource.id) {
 
+              // So this resource is an item and has an id. We can assume that
+              // it has three behaviours and each behaviour has at least one
+              // example.
 
-        function isEmptyItem(resource) {
-          return vm.r.type === 'item' && vm.r.behaviours && !vm.r.behaviours.length;
-        }
+            } else {
 
-        // WHAT IS THAT? WTF
-        // prepopulating empty items?
-        if (isEmptyItem(vm.r)) {
-          vm.behaviours = [];
-          _.each([1, 2, 3], function(i) {
-            vm.behaviours.push({
-              type: 'behaviour',
-              text: '',
-              item_id: vm.r.id,
-              niveau: i,
-              examples: []
-            });
-          });
-          console.log(vm.behaviours);
+              // new items have no id
+
+              vm[resource.type].behaviours = [{
+                text: '',
+                examples: [{
+                  text: ''
+                }]
+              }, {
+                text: '',
+                examples: [{
+                  text: ''
+                }]
+              }, {
+                text: '',
+                examples: [{
+                  text: ''
+                }]
+              }];
+            }
         }
 
         function formIsValid(resource) {
@@ -187,81 +204,112 @@
           Resources.destroy(resource)
             .then(function(response) {
               $mdDialog.hide(response);
-            }, function(error) {});
+            }, function(error) {
+              console.error(error);
+            });
         }
 
-        function addBehaviourToItem(item, behaviour) {}
+        function save(formResource) {
 
-        function addExampleToBehaviour(behaviour, example) {}
+          // Certain resource types (e.g. nested ones) need to be handled a
+          // little bit differently.
 
-        function save(resource) {
+          switch (formResource.type) {
+            case 'item':
+              let item = formResource;
 
-
-
-          // update existing examples
-          _.each(vm.r.examples, function(example) {
-            if (example.hasOwnProperty('id')) {
-
-              Resources.update(example)
-                .then(function(response) {
-                  $mdDialog.hide(response);
+              if (item.hasOwnProperty('id')) {
+                Resources.update(_.pick(item, ['type', 'id', 'subdomain_id', 'name', 'text'])).then(function(response) {
+                  debugger
                 });
 
-            } else {
+                _.each(item.behaviours, function(behaviour, i) {
+                  if (behaviour.hasOwnProperty('id')) {
+                    Resources.update(_.pick(behaviour, ['type', 'id', 'item_id', 'name', 'text', 'niveau'])).then(function(response) {
+                      debugger
+                    });
 
-              Resources.create(example)
-                .then(function(response) {
-                  $mdDialog.hide(response);
-                });
+                    _.each(behaviour.examples, function(example, j) {
+                      if (example.hasOwnProperty('id')) {
+                        Resources.update(_.pick(example, ['type', 'id', 'behaviour_id', 'observation_id', 'name', 'text', 'approved'])).then(function(response) {
+                          $mdDialog.hide(response);
+                        });
+                      } else {
+                        Resources.create(example);
+                      }
+                    });
 
-            }
-          });
-
-          // create new behaviours and examples
-          if (vm.behaviours) {
-            _.each(vm.behaviours, function(behaviour) {
-
-              var examples = behaviour.examples;
-              delete behaviour.examples;
-
-              Resources.create(behaviour)
-                .then(function(response) {
-
-                  var id = response[0].id;
-
-                  _.each(examples, function(example) {
-                    example.behaviour_id = id;
-                    example.type = 'example';
-
-                    Resources.create(example)
+                  } else {
+                    Resources.create(behaviour)
                       .then(function(response) {
-                        $mdDialog.hide(response);
+                        _.each(behaviour.examples, function(example, j) {
+                          example.behaviour_id = response[0].id;
+                          Resources.create(example);
+                        });
                       });
+                  }
+                });
 
+              } else {
+                item.subdomain_id = formResource.subdomain_id;
+                Resources.create(_.pick(item, ['name', 'subdomain_id', 'type'])).then(function(response) {
+                  var item_id = response[0].id;
+                  _.each(item.behaviours, function(behaviour, i) {
+
+                    var newBehaviour = _.extend(_.omit(behaviour, ['examples', '$$hashKey']), {
+                      item_id: item_id,
+                      type: 'behaviour',
+                      niveau: i + 1
+                    });
+
+                    Resources.create(newBehaviour).then(function(response) {
+
+                      var behaviour_id = response[0].id;
+                      _.each(behaviour.examples, function(example, j) {
+
+                        var newExample = _.extend(_.omit(example, ['$$hashKey']), {
+                          behaviour_id: behaviour_id,
+                          type: 'example'
+                        });
+
+                        Resources.create(newExample).then(function(response) {
+                          $mdDialog.hide(response);
+                        });
+                      });
+                    });
                   });
                 });
+              }
 
-            });
+              break;
+
+
+            default:
+
+              // Any regular item is tried to be created if it has no id or
+              // updated if it has one.
+
+              // TODO: We could handle all of this server side: Just post any
+              // resource object to the API and then we could make a check if it
+              // has an id etc. We even could ask the db if the resources exists
+              // and do the appropiate action.
+
+              if (formResource.id) {
+
+                Resources.update(formResource)
+                  .then(function(response) {
+                    $mdDialog.hide(response);
+                  });
+
+              } else {
+
+                Resources.create(formResource)
+                  .then(function(response) {
+                    $mdDialog.hide(response);
+                  });
+
+              }
           }
-
-          if (vm.r.type !== 'item') {
-            if (vm.r.id) {
-
-              Resources.update(resource)
-                .then(function(response) {
-                  $mdDialog.hide(response);
-                });
-
-            } else {
-
-              Resources.create(resource)
-                .then(function(response) {
-                  $mdDialog.hide(response);
-                });
-
-            }
-          }
-
         }
       }
     }
