7fb3e76b1f0c96c74da3eec8c0a8db963c7ae295
René Wilhelm <rene.wilhelm@gmail.com>
2015-03-12 05:44:53 +0100

    Better copy icons script

--- bin/copy_icons.sh
+++ bin/copy_icons.sh
@@ -1,15 +1,7 @@
 #!/usr/local/bin/zsh
-a=($(egrep -ho 'ic_.[a-z]+.*svg' -r app/src | sort -u))
-# echo $a
-for i in $a; do cp bower_components/material-design-icons/**/$i app/dist/img; done
-
-# egrep -rho 'material-design-icons/[a-z0-9/_.]+' app/assets | while read -r l; do cp -v bower_components/$l app/dist/img/$l:t; done
-
-
-# cd app/img
-# mkdir tmp
-# mv $a tmp
-# rm *svg
-# mv tmp/* .
-# cd -
-# ls -l
+test ! -d app/dist/img && mkdir -p app/dist/img
+for i in $(egrep -ho 'ic_.[a-z]+.*svg' -r app/src | sort -u); do
+	if [[ ! -f app/dist/img/$i ]]; then
+		cp bower_components/material-design-icons/**/production/$i app/dist/img;
+	fi
+done
