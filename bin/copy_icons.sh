b11de36c340c415442ef72828e8c5714636e8d37
René Wilhelm <rene.wilhelm@gmail.com>
2015-03-17 23:00:01 +0100

    Fixed icon copy

--- bin/copy_icons.sh
+++ bin/copy_icons.sh
@@ -2,6 +2,6 @@
 test ! -d app/dist/img && mkdir -p app/dist/img
 for i in $(egrep -ho 'ic_.[a-z]+.*svg' -r app/src | sort -u); do
 	if [[ ! -f app/dist/img/$i ]]; then
-		cp bower_components/material-design-icons/**/production/$i app/dist/img;
+		find bower_components/material-design-icons -name $i -exec cp -v -- "{}" app/dist/img \;
 	fi
 done
