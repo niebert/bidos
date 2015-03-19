5839291ce11ae26f26b6241146dc85c170868028
René Wilhelm <rene.wilhelm@gmail.com>
2015-03-19 04:33:34 +0100

    improved run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,30 +1,43 @@
-#!/usr/local/bin/zsh
-
-if ! which tmux >/dev/null; then
-	echo tmux not found; exit 1
-fi
-
-export PORT=3010
-export NODE_ENV=development
+#!/usr/local/bin/zsh -e
+#Thu Mar 19 02:12:45 CET 2015
 
 NAME=bidos
+HOST=$(hostname -f)
 
-tmux new-session -d -s ${NAME}-${NODE_ENV}
+case $NODE_ENV in
+	"production")   PORT=3000  ;;
+	"development")  PORT=3010  ;;
+	"test")         PORT=3020  ;;
+	*)
+		NODE_ENV=development
+		PORT=3010
+		;;
+esac
 
-tmux new-window -k -t ${NAME}:1 -n api
-tmux send-keys -t ${NAME}:1 "NODE_ENV=development gulp api" C-m
+cat << EOF | column -t
+NODE_ENV: $NODE_ENV
+PORT: $PORT
+NAME: $NAME
+HOST: $HOST
+URL: http://$HOST:$PORT
+EOF
 
-tmux new-window -k -t ${NAME}:2 -n www
-tmux send-keys -t ${NAME}:2 "NODE_ENV=development gulp" C-m
+tmux new-session -d -s ${NAME}_${NODE_ENV}
 
-tmux new-window -k -t ${NAME}:3 -n applog
-tmux send-keys -t ${NAME}:3 "tail -F log/${NODE_ENV}.log | ./node_modules/.bin/bunyan -l warn" C-m
+tmux new-window -k -t ${NAME}_${NODE_ENV}:1 -n api
+tmux send-keys -t ${NAME}_${NODE_ENV}:1 "NODE_ENV=$NODE_ENV gulp api" C-m
 
-tmux new-window -k -t ${NAME}:4 -n pglog
-tmux send-keys -t ${NAME}:4 "NODE_ENV=development sudo tail -f /var/log/postgresql/postgresql-9.4-main.log" C-m
+tmux new-window -k -t ${NAME}_${NODE_ENV}:2 -n www
+tmux send-keys -t ${NAME}_${NODE_ENV}:2 "NODE_ENV=$NODE_ENV gulp" C-m
 
-tmux new-window -k -t ${NAME}:5 -n psql
-tmux send-keys -t ${NAME}:5 "psql -U ${NAME} ${NAME}_${NODE_ENV}" C-m
+tmux new-window -k -t ${NAME}_${NODE_ENV}:3 -n applog
+tmux send-keys -t ${NAME}_${NODE_ENV}:3 "tail -F log/${NODE_ENV}.log | ./node_modules/.bin/bunyan -l warn" C-m
 
-tmux attach -t ${NAME}
+tmux new-window -k -t ${NAME}_${NODE_ENV}:4 -n pglog
+tmux send-keys -t ${NAME}_${NODE_ENV}:4 "tail -f /var/log/postgresql/postgresql-9.4-main.log" C-m
+
+tmux new-window -k -t ${NAME}_${NODE_ENV}:5 -n psql
+tmux send-keys -t ${NAME}_${NODE_ENV}:5 "psql -U ${NAME} ${NAME}_${NODE_ENV}" C-m
+
+tmux attach -t ${NAME}_${NODE_ENV}
 
