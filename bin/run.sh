846eceb581823821c3d9cb1ac6997b26218ce346
René Wilhelm <rene.wilhelm@gmail.com>
2015-03-19 01:51:43 +0100

    more stuff in tmux session

--- bin/run.sh
+++ bin/run.sh
@@ -4,18 +4,27 @@ if ! which tmux >/dev/null; then
 	echo tmux not found; exit 1
 fi
 
-export PORT=3002
+export PORT=3010
 export NODE_ENV=development
-local BASEDIR=.
 
-tmux new-session -d -s bidos
+NAME=bidos
 
-tmux new-window -k -t bidos:1 -n api
-tmux send-keys -t bidos:1 "cd $BASEDIR && NODE_ENV=development gulp api" C-m
+tmux new-session -d -s ${NAME}-${NODE_ENV}
 
-tmux new-window -k -t bidos:2 -n www
-tmux send-keys -t bidos:2 "cd $BASEDIR && NODE_ENV=development gulp www" C-m
+tmux new-window -k -t ${NAME}:1 -n api
+tmux send-keys -t ${NAME}:1 "NODE_ENV=development gulp api" C-m
 
-tmux attach -t bidos
+tmux new-window -k -t ${NAME}:2 -n www
+tmux send-keys -t ${NAME}:2 "NODE_ENV=development gulp" C-m
 
- 
+tmux new-window -k -t ${NAME}:3 -n applog
+tmux send-keys -t ${NAME}:3 "tail -F log/${NODE_ENV}.log | ./node_modules/.bin/bunyan -l warn" C-m
+
+tmux new-window -k -t ${NAME}:4 -n pglog
+tmux send-keys -t ${NAME}:4 "NODE_ENV=development sudo tail -f /var/log/postgresql/postgresql-9.4-main.log" C-m
+
+tmux new-window -k -t ${NAME}:5 -n psql
+tmux send-keys -t ${NAME}:5 "psql -U ${NAME} ${NAME}_${NODE_ENV}" C-m
+
+tmux attach -t ${NAME}
+
