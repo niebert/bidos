96043b0bd28e77302fc0b00cfaaa81f257e9b38c
René Wilhelm <rene.wilhelm@gmail.com>
2015-04-03 15:52:54 +0200

    portfolio fixes

--- app/src/core/directives/bx-portfolio/bx-portfolio.js
+++ app/src/core/directives/bx-portfolio/bx-portfolio.js
@@ -12,70 +12,13 @@
       controller: controllerFn,
       controllerAs: 'vm',
       templateUrl: 'templates/bx-portfolio.html',
-      link: linkFn
     };
 
-    function linkFn(scope, element, attrs) {
-
-      var data = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-          label: 'My First dataset',
-          fillColor: 'rgba(220,220,220,0.5)',
-          strokeColor: 'rgba(220,220,220,0.8)',
-          highlightFill: 'rgba(220,220,220,0.75)',
-          highlightStroke: 'rgba(220,220,220,1)',
-          data: [65, 59, 80, 81, 56, 55, 40]
-        }, {
-          label: 'My Second dataset',
-          fillColor: 'rgba(151,187,205,0.5)',
-          strokeColor: 'rgba(151,187,205,0.8)',
-          highlightFill: 'rgba(151,187,205,0.75)',
-          highlightStroke: 'rgba(151,187,205,1)',
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }]
-      };
-
-      var chartOptions = {
-        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-        scaleBeginAtZero: true,
-        //Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines: true,
-        //String - Colour of the grid lines
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-        //Number - Width of the grid lines
-        scaleGridLineWidth: 1,
-        //Boolean - Whether to show horizontal lines (except X axis)
-        scaleShowHorizontalLines: true,
-        //Boolean - Whether to show vertical lines (except Y axis)
-        scaleShowVerticalLines: true,
-        //Boolean - If there is a stroke on each bar
-        barShowStroke: true,
-        //Number - Pixel width of the bar stroke
-        barStrokeWidth: 2,
-        //Number - Spacing between each of the X value sets
-        barValueSpacing: 5,
-        //Number - Spacing between data sets within X values
-        barDatasetSpacing: 1,
-        //String - A legend template
-        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-      };
-
-      var ctx = element.getContext('2d');
-      var myBarChart = new Chart(ctx).Bar(data, chartOptions);
-
-    }
-
     function controllerFn($rootScope, $mdDialog, $scope, Resources) {
-
       var vm = angular.extend(this, {
         auth: $rootScope.auth,
-        select: select,
-        selection: [],
-        isSelected: isSelected,
         selectChart: selectChart
       });
-
 
       vm.tabs = [{
         title: 'nach Kind',
@@ -86,7 +29,7 @@
       }, {
         title: 'nach Institution',
         action: vm.selectChart('institution')
-      }, ];
+      }];
 
       function selectChart(resourceType) {
         switch (resourceType) {
@@ -99,18 +42,6 @@
           case 'institution':
             chartC();
             break;
-        }
-      }
-
-      function isSelected(resource) {
-        return _.contains(vm.selection, resource);
-      }
-
-      function select(resource) {
-        if (_.contains(vm.selection, resource)) {
-          _.remove(vm.selection, resource);
-        } else {
-          vm.selection.push(resource);
         }
       }
 
