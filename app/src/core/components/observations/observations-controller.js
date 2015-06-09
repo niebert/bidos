e3496c184d21e1059aaba4932ebcc6560e3bebe1
René Wilhelm <rene.wilhelm@gmail.com>
2015-06-09 04:01:20 +0200

    Nice observation view

--- app/src/core/components/observations/observations-controller.js
+++ app/src/core/components/observations/observations-controller.js
@@ -1,4 +1,4 @@
-/* global angular */
+/* global _, angular */
 angular.module('bidos')
 .controller('ObservationsController', ObservationsController);
 
@@ -13,20 +13,88 @@ function ObservationsController(Resources, $scope, $rootScope, $mdDialog) {
       controller: 'ObservationDialogController',
       templateUrl: `templates/observation.dialog.view.html`
     }).then(function() {
-      $scope.reset();
+      // $scope.reset();
     }, function() {
     });
   };
+  $scope.stuff = {};
+  $scope.me = $rootScope.me;
+  Resources.get().then(function(data) {
 
-  $scope.observations = $rootScope.data.observations.filter(function(observation) {
-    if (!observation) return false;
+    $scope.kids = data.kids;
+    $scope.groups = data.groups;
+    $scope.domains = data.domains;
 
-    if (!observation.hasOwnProperty('author_id')) {
-      observation.author_id = -1;
+    $scope.observations = data.observations;
+
+    if ($scope.me.role === 2) {
+      $scope.observations = _.filter(data.observations);
+      // $scope.observations = _.filter(data.observations, {approved: false});
+    } else {
+      $scope.observations = _.filter(data.observations, {author_id: $rootScope.me.id});
     }
 
-    return (observation.author_id === $rootScope.me.id)
-      && (observation.kid.group_id === $rootScope.me.group_id); // admin sees all observations
   });
+
+  $scope.resetFilters = function() {
+    $scope.stuff = {};
+  };
+
+  $scope.myFilter = function(observation) {
+    let q = [];
+
+    if ($scope.stuff.kid_id) {
+      q.push(compareNum($scope.stuff.kid_id, observation.kid_id));
+    }
+
+    if ($scope.stuff.group_id) {
+      q.push(compareNum($scope.stuff.group_id, observation.kid.group.id));
+    }
+
+    if ($scope.stuff.domain_id) {
+      q.push(compareNum($scope.stuff.domain_id, observation.item.subdomain.domain.id));
+    }
+
+    if ($scope.stuff.unapproved) {
+      q.push(observation.approved === false);
+    }
+
+    // if ($scope.stuff.id) {
+    //   q.push(compareNum($scope.stuff.id, arguments[0].id));
+    // }
+
+    // if ($scope.stuff.name) {
+    //   q.push(compareString($scope.stuff.name, arguments[0].name));
+    // }
+
+    // if ($scope.stuff.title) {
+    //   q.push(compareString($scope.stuff.title, arguments[0].title));
+    // }
+
+    // if ($scope.stuff.group && arguments[0].group) {
+    //   q.push(compareString($scope.stuff.group, (arguments[0].group.name || '')));
+    // }
+
+    // if ($scope.stuff.institution) {
+    //   q.push(compareString($scope.stuff.institution, arguments[0].institution.name));
+    // }
+
+    return _.all(q);
+
+    function compareNum(a, b) {
+      if (a && b) {
+        return parseInt(a) === parseInt(b);
+      }
+      return true;
+    }
+
+    function compareString(a, b) {
+      if (a && b) {
+        return b.match(new RegExp(a, 'gi'));
+      }
+      return true;
+    }
+  };
+
 
 }
