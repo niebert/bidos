0a858644e868c94a5296e6b7ddaaf6500eeadb3b
René Wilhelm <rene.wilhelm@gmail.com>
2015-06-16 16:31:22 +0200

    [tabbar] polished titles

--- app/src/core/components/tabbar/tabbar-controller.js
+++ app/src/core/components/tabbar/tabbar-controller.js
@@ -5,33 +5,37 @@ angular.module('bidos')
 function TabbarController($scope, $state, Resources) {
 
   $scope.actions = [{
-    text: 'Home',
+    text: 'Übersicht',
     roles: ['Praktiker', 'Administrator', 'Wissenschaftler'],
     onClick: function () {
       $state.go('bidos.home');
     }
   }, {
-    text: 'Meine Kinder',
+    text: 'Kinder',
     roles: ['Praktiker'],
     onClick: function () {
+      console.log('bidos.kids');
       $state.go('bidos.kids');
     }
   }, {
-    text: 'Meine Beobachtungen',
+    text: 'Beobachtungen',
     roles: ['Praktiker'],
     onClick: function () {
+      console.log('bidos.observations');
       $state.go('bidos.observations');
     }
   }, {
     text: 'Neue Beobachtung',
     roles: ['Praktiker'],
     onClick: function () {
+      console.log('bidos.capture');
       $state.go('bidos.capture');
     }
   }, {
     text: 'Benutzer',
     roles: ['Administrator'],
     onClick: function () {
+      console.log('bidos.users');
       $state.go('bidos.users');
     }
   // }, {
@@ -53,7 +57,7 @@ function TabbarController($scope, $state, Resources) {
       $state.go('bidos.domains');
     }
   }, {
-    text: 'Gruppe auswerten',
+    text: 'Auswertung',
     roles: ['Praktiker'],
     onClick: function () {
       $state.go('bidos.charts');
@@ -65,7 +69,7 @@ function TabbarController($scope, $state, Resources) {
       $state.go('bidos.observations');
     }
   }, {
-    text: 'Items',
+    text: 'Bausteine',
     roles: ['Administrator'],
     onClick: function () {
       $state.go('bidos.items');
