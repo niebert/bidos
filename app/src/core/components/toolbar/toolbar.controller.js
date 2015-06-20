50d47da79b007d6c53c11743f9f7b68aa10cdda7
René Wilhelm <rene.wilhelm@gmail.com>
2015-06-21 00:10:32 +0200

    Improved main menu

--- app/src/core/components/toolbar/toolbar.controller.js
+++ app/src/core/components/toolbar/toolbar.controller.js
@@ -1,3 +1,38 @@
+/* global angular */
+angular.module('bidos')
+  .controller('Toolbar', Toolbar);
+
+  function Toolbar(Resources, $scope, $rootScope, $mdDialog) {
+
+    Resources.get().then(function(data) {
+      $scope.me = data.me;
+    });
+
+    $scope.settings = function (ev) {
+      $mdDialog.show({
+        targetEvent: ev,
+        controller: 'SettingsDialog',
+        templateUrl: 'templates/settings-dialog.html'
+      });
+    };
+
+    $scope.sync = function () {
+      Resources.init();
+    };
+
+    $scope.account = function (ev) {
+      $mdDialog.show({
+        targetEvent: ev,
+        locals: {me: $scope.me},
+        controller: 'AccountDialog',
+        templateUrl: 'templates/account-dialog.html'
+      });
+    };
+
+    $scope.docs = function (ev) {
+
+    };
+
     $scope.feedback = function (ev) {
       $mdDialog.show({
         targetEvent: ev,
@@ -7,3 +42,20 @@
       });
     };
 
+    $scope.about = function (ev) {
+      $mdDialog.show({
+        targetEvent: ev,
+        locals: {me: $scope.me},
+        controller: 'AboutDialog',
+        templateUrl: 'templates/about-dialog.html'
+      });
+    };
+
+    $scope.logout = function (ev) {
+      $mdDialog.show({
+        targetEvent: ev,
+        controller: 'LogoutDialog',
+        templateUrl: 'templates/logout-dialog.html'
+      });
+    };
+  }
