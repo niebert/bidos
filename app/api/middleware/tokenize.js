bde6a7be6dae437a707ac84848209629df7c4bbe
René Wilhelm <rene.wilhelm@gmail.com>
2015-06-21 02:58:49 +0200

    Reduced auth token exp to 8 hours

--- app/api/middleware/tokenize.js
+++ app/api/middleware/tokenize.js
@@ -8,7 +8,8 @@ const secret = require('../config').secret;
 function* tokenize() {
   this.body = _.merge(this.user, {
     token: jwt.sign(this.user, secret, {
-      expiresInMinutes: 60 * 24 * 7 // one week
+      user_id: this.user.id,
+      expiresInMinutes: 60 * 8 // eight hours
     })
   });
 }
