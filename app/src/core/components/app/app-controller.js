4b94b4752e74618c1feb85ed6166ca7d7372a834
René Wilhelm <rene.wilhelm@gmail.com>
2015-07-02 02:00:05 +0200

    Removed app controller/container

--- app/src/core/components/app/app-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/* global angular */
-angular.module('bidos')
-.controller('AppController', AppController);
-
-function AppController(Resources) {
-	console.log('init resources in app controller');
-  Resources.init();
-}
