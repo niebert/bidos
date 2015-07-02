a205181416e1e15eeba496d67aefd50b6b770ba9
René Wilhelm <rene.wilhelm@gmail.com>
2015-07-02 02:06:12 +0200

    Longer token expire delay

--- app/api/middleware/tokenize.js
+++ app/api/middleware/tokenize.js
@@ -9,7 +9,7 @@ function* tokenize() {
   this.body = _.merge(this.user, {
     token: jwt.sign(this.user, secret, {
       user_id: this.user.id,
-      expiresInMinutes: 60 * 8 // eight hours
+      expiresInMinutes: 60 * 24 * 14 // two weeks
     })
   });
 }
